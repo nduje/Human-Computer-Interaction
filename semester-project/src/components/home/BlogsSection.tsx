a47d01950d7a30af95e5fdf34c4ded06ff1d2790
Duje Nikolić <dujerancic@gmail.com>
2024-08-17 19:41:37 +0200

    Add Blogs Section

---
+++ semester-project/src/components/home/BlogsSection.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import thompson from "../images/blogs/thompson.jpeg"
+import oliver from "../images/blogs/oliver.jpg"
+import prljavo_kazaliste from "../images/blogs/prljavo_kazaliste.jpeg"
+import solin_summer_festival from "../images/blogs/solin_summer_festival.jpg"
+
+const BlogsSection: FC = () => {
+    return (
+        <article className="flex font-roboto flex-col text-center align-middle justify-center bg-base-colors-50 m-12">
+            <h1 className="font-bold text-3xl text-base-colors-200 m-12">
+                Blogs
+            </h1>
+            <section className="grid grid-cols-3 gap-14 m-8 mx-20">
+                <section className="flex flex-col justify-center align-middle items-center text-base-colors-200 hover:cursor-pointer hover:text-base-colors-300">
+                    <Image
+                        style={{width: "100%", height: "225px", objectFit: "cover"}}
+                        src={thompson}
+                        alt="thompson"
+                        className="rounded-t-md object-cover"
+                    />
+                    <div className="flex justify-start text-left items-center align-middle rounded-b-md bg-base-colors-100 h-16 w-full p-4">
+                        <h1 className="font-medium">
+                            Thompson held a concert in front of 40,000 people
+                        </h1>
+                    </div>
+                </section>
+                <section className="flex flex-col justify-center align-middle items-center text-base-colors-200 hover:cursor-pointer hover:text-base-colors-300">
+                    <Image
+                        style={{width: "100%", height: "225px", objectFit: "cover"}}
+                        src={oliver}
+                        alt="oliver"
+                        className="rounded-t-md"
+                    />
+                    <div className="flex justify-start text-left items-center align-middle rounded-b-md bg-base-colors-100 h-16 w-full p-4">
+                        <h1 className="font-medium">
+                            The sixth anniversary of Oliver's death was commemorated
+                        </h1>
+                    </div>
+                </section>
+                <section className="flex flex-col justify-center align-middle items-center text-base-colors-200 hover:cursor-pointer hover:text-base-colors-300">
+                    <Image
+                        style={{width: "100%", height: "225px", objectFit: "cover"}}
+                        src={solin_summer_festival}
+                        alt="solin_summer_festival"
+                        className="rounded-t-md object-cover"
+                    />
+                    <div className="flex justify-start text-left items-center align-middle rounded-b-md bg-base-colors-100 h-16 w-full p-4">
+                        <h1 className="font-medium">
+                            29th Solin Summer Festival: Program Announcement
+                        </h1>
+                    </div>
+                </section>
+                <section className="flex flex-col justify-center align-middle items-center text-base-colors-200 hover:cursor-pointer hover:text-base-colors-300">
+                    <Image
+                        style={{width: "100%", height: "225px", objectFit: "cover"}}
+                        src={prljavo_kazaliste}
+                        alt="prljavo_kazaliste"
+                        className="rounded-t-md object-cover"
+                    />
+                    <div className="flex justify-start text-left items-center align-middle rounded-b-md bg-base-colors-100 h-16 w-full p-4">
+                        <h1 className="font-medium">
+                            "Prljavci" announced a concert in Zagreb
+                        </h1>
+                    </div>
+                </section>
+            </section>
+        </article>
+    );
+};
+
+export default BlogsSection;
--- semester-project/src/components/home/CategoriesSection.tsx
+++ semester-project/src/components/home/CategoriesSection.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { FC } from "react";
+import Link from "next/link";
 import Image from "next/image";
 import guitars from "../images/guitars.jpg";
 import drums from "../images/drums.jpg";
@@ -21,7 +22,7 @@ const HeadlineSection: FC = () => {
         <section className="flex flex-row justify-evenly text-center align-middle h-3/4">
             <section className="flex flex-col justify-evenly text-center align-middle h-full w-1/4 m-2 p-2">
                 <div className="flex flex-col justify-between text-center align-middle">
-                    <div className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
+                    <Link href="music-instruments/guitars" className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
                         <Image
                             width={75}
                             height={75}
@@ -32,26 +33,11 @@ const HeadlineSection: FC = () => {
                         <h1 className="my-auto text-xl font-semibold">
                             Guitars
                         </h1>
-                    </div>
+                    </Link>
                     <hr className="border-[1.5px] w-full border-base-colors-100 my-4"></hr>
                 </div>  
                 <div className="flex flex-col justify-between text-center align-middle">
-                    <div className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
-                        <Image
-                            width={75}
-                            height={75}
-                            src={drums}
-                            alt="drums"
-                            className="rounded-md"
-                        />
-                        <h1 className="my-auto text-xl font-semibold">
-                            Drums
-                        </h1>
-                    </div>
-                    <hr className="border-[1.5px] w-full border-base-colors-100 my-4"></hr>
-                </div>  
-                <div className="flex flex-col justify-between text-center align-middle">
-                    <div className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
+                    <Link href="music-instruments/keyboards-and-piano" className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
                         <Image
                             width={75}
                             height={75}
@@ -62,13 +48,28 @@ const HeadlineSection: FC = () => {
                         <h1 className="my-auto text-xl font-semibold">
                             Keys
                         </h1>
-                    </div>
+                    </Link>
+                    <hr className="border-[1.5px] w-full border-base-colors-100 my-4"></hr>
+                </div>  
+                <div className="flex flex-col justify-between text-center align-middle">
+                    <Link href="music-instruments/drums-and-percussion" className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
+                        <Image
+                            width={75}
+                            height={75}
+                            src={drums}
+                            alt="drums"
+                            className="rounded-md"
+                        />
+                        <h1 className="my-auto text-xl font-semibold">
+                            Drums
+                        </h1>
+                    </Link>
                     <hr className="border-[1.5px] w-full border-base-colors-100 my-4"></hr>
                 </div>  
             </section>
             <section className="flex flex-col justify-evenly text-center align-middle w-1/4 m-2 p-2">
                 <div className="flex flex-col justify-between text-center align-middle">
-                    <div className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
+                    <Link href="/audio-equipment/amplifiers" className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
                         <Image
                             width={75}
                             height={75}
@@ -79,11 +80,11 @@ const HeadlineSection: FC = () => {
                         <h1 className="my-auto text-xl font-semibold">
                             Amplifiers
                         </h1>
-                    </div>
+                    </Link>
                     <hr className="border-[1.5px] w-full border-base-colors-100 my-4"></hr>
                 </div>  
                 <div className="flex flex-col justify-between text-center align-middle">
-                    <div className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
+                    <Link href="/audio-equipment/microphones" className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
                         <Image
                             width={75}
                             height={75}
@@ -94,11 +95,11 @@ const HeadlineSection: FC = () => {
                         <h1 className="my-auto text-xl font-semibold">
                             Microphones
                         </h1>
-                    </div>
+                    </Link>
                     <hr className="border-[1.5px] w-full border-base-colors-100 my-4"></hr>
                 </div>  
                 <div className="flex flex-col justify-between text-center align-middle">
-                    <div className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
+                    <Link href="/audio-equipment/pedals" className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
                         <Image
                             width={75}
                             height={75}
@@ -109,13 +110,13 @@ const HeadlineSection: FC = () => {
                         <h1 className="my-auto text-xl font-semibold">
                             Pedals
                         </h1>
-                    </div>
+                    </Link>
                     <hr className="border-[1.5px] w-full border-base-colors-100 my-4"></hr>
                 </div>       
             </section>
             <section className="flex flex-col justify-evenly text-center align-middle w-1/4 m-2 p-2">
                 <div className="flex flex-col justify-between text-center align-middle">
-                    <div className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
+                    <Link href="/audio-equipment/dj-equipment" className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
                         <Image
                             width={75}
                             height={75}
@@ -126,11 +127,11 @@ const HeadlineSection: FC = () => {
                         <h1 className="my-auto text-xl font-semibold">
                             DJ
                         </h1>
-                    </div>
+                    </Link>
                     <hr className="border-[1.5px] w-full border-base-colors-100 my-4"></hr>
                 </div>  
                 <div className="flex flex-col justify-between text-center align-middle">
-                    <div className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
+                    <Link href="resources-and-learning/sheet-music" className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
                         <Image
                             width={75}
                             height={75}
@@ -141,11 +142,11 @@ const HeadlineSection: FC = () => {
                         <h1 className="my-auto text-xl font-semibold">
                             Sheet
                         </h1>
-                    </div>
+                    </Link>
                     <hr className="border-[1.5px] w-full border-base-colors-100 my-4"></hr>
                 </div>  
                 <div className="flex flex-col justify-between text-center align-middle">
-                    <div className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
+                    <Link href="resources-and-learning/vinyl-records" className="flex flex-auto justify-between text-center align-middle m-4 px-10 hover:cursor-pointer text-base-colors-200 hover:text-base-colors-300">
                         <Image
                             width={75}
                             height={75}
@@ -156,7 +157,7 @@ const HeadlineSection: FC = () => {
                         <h1 className="my-auto text-xl font-semibold">
                             Vinyls
                         </h1>
-                    </div>
+                    </Link>
                     <hr className="border-[1.5px] w-full border-base-colors-100 my-4"></hr>
                 </div>  
             </section>
