21a4c275c1fe81326030e5eae113b4395d1573a8
Duje Nikolić <dujerancic@gmail.com>
2024-08-26 12:55:58 +0200

    Remove Odd Files

--- semester-project/src/app/sort/login/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function LoginPage() {
-  return (
-    <div className="flex justify-center align-middle font-bold text-4xl">
-      <h1>Login</h1>
-    </div>
-  );
-}
--- semester-project/src/app/sort/sheet-music/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function SheetMusicPage() {
-  return (
-    <div className="flex justify-center align-middle font-bold p-14 text-4xl">
-      <h1>Sheet Music</h1>
-    </div>
-  );
-}
